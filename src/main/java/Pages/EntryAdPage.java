6f153bd771484afbfb3fb8f54064df70ca7ce553
Ramzy Fenhas <ramzy.fenhas@gamil.com>
2024-01-26 10:50:44 +0200

    Session 5 (Frame,Modal,JSAlerts)

--- src/main/java/Pages/EntryAdPage.java
+++ src/main/java/Pages/EntryAdPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class EntryAdPage {
     WebDriver driver;
     WebDriverWait wait;
@@ -12,13 +14,14 @@ public class EntryAdPage {
         this.driver = driver;
     }
 
-    private final By closeButton = By.linkText("Close");
-
+    private final By closeButton = By.cssSelector(".modal-footer > p");
     private final By clickHereButton = By.linkText("click here");
 
 
 
     public void clickOnCloseButton(){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(closeButton));
         driver.findElement(closeButton).click();
     }
     public void clickOnClickHereButton(){
--- src/test/java/modal/ModalTests.java
+++ src/test/java/modal/ModalTests.java
@@ -4,32 +4,30 @@ import Pages.EntryAdPage;
 import base.BaseTests;
 import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.OutputType;
 import org.testng.annotations.Test;
 
-import java.util.Base64;
 
 public class ModalTests extends BaseTests {
 
     @Test
-    public void validateOnModal(){
+    public void validateOnModal() {
         boolean presence = false;
         EntryAdPage entryAdPage = homePage.clickOnEntryAdLink();
         try {
             entryAdPage.clickOnCloseButton();
 
-        } catch (NoSuchElementException e) {
+        } catch (NoSuchElementException | ElementNotInteractableException e) {
             presence = true;
             while (presence == true){
-                if (presence = true) {
                     driver.get("https://the-internet.herokuapp.com/");
                     homePage.clickOnEntryAdLink();
                     try {
                         entryAdPage.clickOnCloseButton();
-                    } catch (NoSuchElementException ex) {
-                    }
-                    presence = false;
-                }else { presence = true;}
+                        presence = false;
+                    } catch (NoSuchElementException | ElementNotInteractableException ex) {
+
+
+                }
             }
         }
 
